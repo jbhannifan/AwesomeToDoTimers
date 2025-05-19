baf6cb6a31ad547d031a51f8c2c968a774668d47
jbhannifan <john.hannifan@gmail.com>
2025-05-19 10:21:29 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
-  root: '.', // Use current folder as root
-  build: {
-    outDir: 'dist',
-  }
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});
